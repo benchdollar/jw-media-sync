7faadcc8592697fb3d2b6d2c3d378e19d8885713
Benjamin Knoop <bennyx@gmx.de>
2021-11-10 01:20:31 +0100

    Autocomplete form elements with selectable data

--- src/renderer/components/OptionsForm.tsx
+++ src/renderer/components/OptionsForm.tsx
@@ -1,18 +1,95 @@
 import { FolderOpen } from '@mui/icons-material';
-import { Button, IconButton, Stack, TextField } from '@mui/material';
+import { Autocomplete, Button, Stack, TextField } from '@mui/material';
+import { useState } from 'react';
+
+interface CodeItem {
+  code: string;
+  name: string;
+}
+
+function label(item: CodeItem): string {
+  return `${item.name} (${item.code})`;
+}
+
+const publications: Array<CodeItem> = [
+  { code: 'pkon', name: 'Children Songs' },
+  { code: 'pk', name: 'Become Jehovah’s Friend' },
+  { code: 'pksjj', name: 'Become Jehovah’s Friend—Sing With Us' },
+  { code: 'sjjm', name: '"Sing Out Joyfully" to Jehovah — Meetings' },
+  { code: 'sjjc', name: '"Sing Out Joyfully" to Jehovah — Vocals' },
+  { code: 'sjji', name: '"Sing Out Joyfully" to Jehovah — Instrumental' },
+  { code: 'osg', name: 'Original Songs' },
+];
+
+const filetypes: Array<string> = ['MP4', 'MP3'];
+
+const languages: Array<CodeItem> = [
+  { code: 'X', name: 'German' },
+  { code: 'E', name: 'English' },
+  { code: 'U', name: 'Russian' },
+];
 
 export default function OptionsForm() {
+  const [publicationSymbol, setPublicationSymbol] = useState('');
+  const [filetype, setFiletype] = useState(filetypes[0]);
+  const [language, setLanguage] = useState(label(languages[0]));
+
   return (
     <Stack spacing={2}>
       <Stack direction="row" spacing={2}>
-        <TextField
-          id="publication-code"
-          label="Publication"
-          variant="filled"
+        <Autocomplete
+          value={publicationSymbol}
+          inputValue={publicationSymbol}
+          disablePortal
+          id="publication-symbol"
+          options={...publications.map((item) => {
+            return label(item);
+          })}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              variant="filled"
+              label="Publication Symbol"
+            />
+          )}
+          onInputChange={(_event, newInputValue) => {
+            setPublicationSymbol(newInputValue);
+          }}
           fullWidth
+          freeSolo
         />
-        <TextField id="filetype" label="File Type" variant="filled" fullWidth />
-        <TextField id="language" label="Language" variant="filled" fullWidth />
+        <Autocomplete
+          value={filetype}
+          inputValue={filetype}
+          disablePortal
+          id="filetype"
+          options={filetypes}
+          renderInput={(params) => (
+            <TextField {...params} variant="filled" label="File Type" />
+          )}
+          onInputChange={(_event, newInputValue) => {
+            setFiletype(newInputValue);
+          }}
+          disableClearable
+          sx={{ minWidth: 120 }}
+        />
+        <Autocomplete
+          value={language}
+          inputValue={language}
+          disablePortal
+          id="language-code"
+          options={...languages.map((item) => {
+            return label(item);
+          })}
+          renderInput={(params) => (
+            <TextField {...params} variant="filled" label="Language Code" />
+          )}
+          onInputChange={(_event, newInputValue) => {
+            setLanguage(newInputValue);
+          }}
+          fullWidth
+          freeSolo
+        />
       </Stack>
       <Stack direction="row" spacing={2}>
         <TextField
